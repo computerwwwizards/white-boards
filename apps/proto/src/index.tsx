fafb1dd43e21f0bafba41592b654c348acb6eb34
shadow <shadow8095@gmail.com>
2025-09-30 19:07:45 -0500

    fix: base url worker

--- apps/proto/src/index.tsx
+++ apps/proto/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 if ('serviceWorker' in navigator) {
 
-  const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
+  const swUrl = `${import.meta.env.BASE_URL}/service-worker.js`;
   navigator.serviceWorker.register(swUrl)
     .then(reg => {
       console.log('Service Worker registered:', reg);
